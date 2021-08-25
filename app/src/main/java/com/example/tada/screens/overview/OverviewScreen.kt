da1be540d966c508c6f530e194b09b51ca7b3caf
Leon Fuessner <leon.fuessner@hotmail.de>
2021-08-25 15:30:26 +0200

    Imported icons and improved composables

--- app/src/main/java/com/example/tada/screens/overview/OverviewScreen.kt
+++ app/src/main/java/com/example/tada/screens/overview/OverviewScreen.kt
@@ -1,10 +1,12 @@
 package com.example.tada.screens.overview
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -13,15 +15,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import com.example.tada.R
-import com.example.tada.TadaScreen
 import com.example.tada.model.Category
+import com.example.tada.ui.theme.icons
+import java.util.*
+import kotlin.random.Random
 
 @Composable
 fun OverviewScreen(
@@ -50,26 +52,40 @@ fun OverviewContent(
         modifier = Modifier
             .fillMaxSize()
     ) {
+        BackgroundCircle()
         Column {
             Spacer(modifier = Modifier.height(20.dp))
             OverviewHeader()
             Spacer(modifier = Modifier.height(20.dp))
             OverviewList(
                 categories = categories,
-                onCategoryClick = onCategoryClick
+                onCategoryClick = onCategoryClick,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
             )
         }
     }
 }
 
 @Composable
+fun BackgroundCircle() {
+    Box(
+        modifier = Modifier
+            .absoluteOffset(y = (-500).dp)
+            .size(400.dp)
+            .clip(RoundedCornerShape(80.dp))
+            .background(MaterialTheme.colors.primary)
+    )
+}
+
+@Composable
 fun OverviewHeader() {
     Column(
         modifier = Modifier
+            .padding(start = 36.dp, top = 20.dp)
             .fillMaxWidth(0.8f)
-            .padding(20.dp)
     ) {
-        Text(style = MaterialTheme.typography.h4, text = "Hello there")
+        Text(style = MaterialTheme.typography.h5, text = "Hello there")
         Text(style = MaterialTheme.typography.body1, text = "Today you have 4 tasks")
     }
 }
@@ -79,12 +95,14 @@ fun OverviewHeader() {
 @Composable
 fun OverviewList(
     categories: List<Category>,
-    onCategoryClick: (id: Long) -> Unit
+    onCategoryClick: (id: Long) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     LazyColumn(
-        modifier = Modifier.fillMaxHeight(),
+        modifier = modifier
+            .fillMaxHeight(),
         contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.spacedBy(32.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         items(categories) { category ->
             CategoryItem(
@@ -104,49 +122,54 @@ fun CategoryItem(
 ) {
     Box(
         modifier = modifier
-            .shadow(16.dp, RoundedCornerShape(16.dp))
+            .shadow(8.dp, RoundedCornerShape(16.dp))
             .clip(RoundedCornerShape(16.dp))
             .height(124.dp)
             .fillMaxWidth(0.9f)
+            .background(Color.White)
             .clickable { onCategoryClick.invoke(category.id) },
     ) {
         Row(
-            modifier = modifier
-
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .align(Alignment.CenterStart)
         ) {
+            Image(
+                painterResource(id = icons[Random.nextInt(icons.size)]),
+                contentDescription = "bathtub",
+                modifier = Modifier
+                    .size(48.dp)
+                    .align(Alignment.CenterVertically)
+            )
+
             Column(
-                modifier = modifier.align(Alignment.CenterVertically)
-            ) {
-                Image(
-                    painterResource(id = R.drawable.ic_baseline_bathtub_24),
-                    contentDescription = "bathtub",
-                    modifier = Modifier
-                        .size(48.dp)
-                )
-            }
-            Column(
-                modifier = modifier.align(Alignment.CenterVertically)
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .align(Alignment.CenterVertically)
             ) {
                 Text(
+
                     style = MaterialTheme.typography.h6,
                     text = category.title
                 )
                 Text(
-                    style = MaterialTheme.typography.body1,
+                    style = MaterialTheme.typography.subtitle2,
+                    color = Color.LightGray,
                     text = "${category.tasks.size} Tasks"
                 )
             }
-            Column(
-            ) {
-                Image(
-                    painterResource(id = R.drawable.ic_baseline_more_vert_24),
-                    contentDescription = "more",
-                    modifier = Modifier
-                        .size(48.dp)
-                )
-            }
         }
+
+        Image(
+            painterResource(id = R.drawable.ic_baseline_more_vert_24),
+            contentDescription = "more",
+            modifier = Modifier
+                .padding(8.dp)
+                .size(32.dp)
+                .align(Alignment.TopEnd)
+        )
     }
-
-
 }
+
+
+
---
+++ app/src/main/java/com/example/tada/ui/theme/Icons.kt
@@ -0,0 +1,41 @@
+package com.example.tada.ui.theme
+
+import android.graphics.drawable.Drawable
+import com.example.tada.R
+
+val icons: List<Int>
+    get() = listOf(
+        R.drawable.ic_almond,
+        R.drawable.ic_avocado_fruit,
+        R.drawable.ic_banana,
+        R.drawable.ic_beet,
+        R.drawable.ic_bell_pepper,
+        R.drawable.ic_blueberry,
+        R.drawable.ic_cabbage,
+        R.drawable.ic_carrot,
+        R.drawable.ic_cashew,
+        R.drawable.ic_cherries,
+        R.drawable.ic_chili,
+        R.drawable.ic_coconut,
+        R.drawable.ic_coconut_with_straw,
+        R.drawable.ic_corn,
+        R.drawable.ic_cucumber,
+        R.drawable.ic_dry_grapes,
+        R.drawable.ic_durian_fruit,
+        R.drawable.ic_eggplant,
+        R.drawable.ic_fresh_apple,
+        R.drawable.ic_garlic,
+        R.drawable.ic_green_grapes,
+        R.drawable.ic_kiwi_food,
+        R.drawable.ic_mango_fruit,
+        R.drawable.ic_onion,
+        R.drawable.ic_orange,
+        R.drawable.ic_peach_fruit,
+        R.drawable.ic_pineapple_fruit,
+        R.drawable.ic_potato,
+        R.drawable.ic_pumpkin,
+        R.drawable.ic_purple_grapes,
+        R.drawable.ic_tender_coconut,
+        R.drawable.ic_tomato,
+        R.drawable.ic_watermelon_fruit
+    )
